87f9a9609053ad069aa7f6508d55f154e4819099
Alclo <alcloponso@gmail.com>
2026-05-05 23:50:39 -0600

    BDPrueba1

---
+++ derechohabitante_dev.sql
@@ -0,0 +1,368 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.14
+-- Dumped by pg_dump version 16.0
+
+-- Started on 2026-05-05 23:48:38
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 225 (class 1259 OID 25732)
+-- Name: batch_error_logs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_error_logs (
+    id integer NOT NULL,
+    job_name character varying(100),
+    file_name character varying(255),
+    raw_data text,
+    error_message text,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.batch_error_logs OWNER TO postgres;
+
+--
+-- TOC entry 224 (class 1259 OID 25731)
+-- Name: batch_error_logs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_error_logs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_error_logs_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3374 (class 0 OID 0)
+-- Dependencies: 224
+-- Name: batch_error_logs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.batch_error_logs_id_seq OWNED BY public.batch_error_logs.id;
+
+
+--
+-- TOC entry 216 (class 1259 OID 25670)
+-- Name: batch_job_execution; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_execution (
+    job_execution_id bigint NOT NULL,
+    version bigint,
+    job_instance_id bigint NOT NULL,
+    create_time timestamp without time zone NOT NULL,
+    start_time timestamp without time zone,
+    end_time timestamp without time zone,
+    status character varying(10),
+    exit_code character varying(2500),
+    exit_message character varying(2500),
+    last_updated timestamp without time zone
+);
+
+
+ALTER TABLE public.batch_job_execution OWNER TO postgres;
+
+--
+-- TOC entry 220 (class 1259 OID 25716)
+-- Name: batch_job_execution_context; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_execution_context (
+    job_execution_id bigint NOT NULL,
+    short_context character varying(2500) NOT NULL,
+    serialized_context text
+);
+
+
+ALTER TABLE public.batch_job_execution_context OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 25682)
+-- Name: batch_job_execution_params; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_execution_params (
+    job_execution_id bigint NOT NULL,
+    parameter_name character varying(100) NOT NULL,
+    parameter_type character varying(100) NOT NULL,
+    parameter_value character varying(2500),
+    identifying character(1) NOT NULL
+);
+
+
+ALTER TABLE public.batch_job_execution_params OWNER TO postgres;
+
+--
+-- TOC entry 222 (class 1259 OID 25729)
+-- Name: batch_job_execution_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_job_execution_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_job_execution_seq OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 25663)
+-- Name: batch_job_instance; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_instance (
+    job_instance_id bigint NOT NULL,
+    version bigint,
+    job_name character varying(100) NOT NULL,
+    job_key character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.batch_job_instance OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 25730)
+-- Name: batch_job_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_job_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_job_seq OWNER TO postgres;
+
+--
+-- TOC entry 218 (class 1259 OID 25692)
+-- Name: batch_step_execution; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_step_execution (
+    step_execution_id bigint NOT NULL,
+    version bigint NOT NULL,
+    step_name character varying(100) NOT NULL,
+    job_execution_id bigint NOT NULL,
+    create_time timestamp without time zone NOT NULL,
+    start_time timestamp without time zone,
+    end_time timestamp without time zone,
+    status character varying(10),
+    commit_count bigint,
+    read_count bigint,
+    filter_count bigint,
+    write_count bigint,
+    read_skip_count bigint,
+    write_skip_count bigint,
+    process_skip_count bigint,
+    rollback_count bigint,
+    exit_code character varying(2500),
+    exit_message character varying(2500),
+    last_updated timestamp without time zone
+);
+
+
+ALTER TABLE public.batch_step_execution OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 25704)
+-- Name: batch_step_execution_context; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_step_execution_context (
+    step_execution_id bigint NOT NULL,
+    short_context character varying(2500) NOT NULL,
+    serialized_context text
+);
+
+
+ALTER TABLE public.batch_step_execution_context OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 25728)
+-- Name: batch_step_execution_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_step_execution_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_step_execution_seq OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 25556)
+-- Name: derechohambientes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.derechohambientes (
+    id integer NOT NULL,
+    nombre character varying(100) NOT NULL,
+    ciudad character varying(100),
+    importe numeric(19,2),
+    cuenta character varying(16) NOT NULL
+);
+
+
+ALTER TABLE public.derechohambientes OWNER TO postgres;
+
+--
+-- TOC entry 3204 (class 2604 OID 25735)
+-- Name: batch_error_logs id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_error_logs ALTER COLUMN id SET DEFAULT nextval('public.batch_error_logs_id_seq'::regclass);
+
+
+--
+-- TOC entry 3221 (class 2606 OID 25740)
+-- Name: batch_error_logs batch_error_logs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_error_logs
+    ADD CONSTRAINT batch_error_logs_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3219 (class 2606 OID 25722)
+-- Name: batch_job_execution_context batch_job_execution_context_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution_context
+    ADD CONSTRAINT batch_job_execution_context_pkey PRIMARY KEY (job_execution_id);
+
+
+--
+-- TOC entry 3213 (class 2606 OID 25676)
+-- Name: batch_job_execution batch_job_execution_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution
+    ADD CONSTRAINT batch_job_execution_pkey PRIMARY KEY (job_execution_id);
+
+
+--
+-- TOC entry 3209 (class 2606 OID 25667)
+-- Name: batch_job_instance batch_job_instance_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_instance
+    ADD CONSTRAINT batch_job_instance_pkey PRIMARY KEY (job_instance_id);
+
+
+--
+-- TOC entry 3217 (class 2606 OID 25710)
+-- Name: batch_step_execution_context batch_step_execution_context_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution_context
+    ADD CONSTRAINT batch_step_execution_context_pkey PRIMARY KEY (step_execution_id);
+
+
+--
+-- TOC entry 3215 (class 2606 OID 25698)
+-- Name: batch_step_execution batch_step_execution_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution
+    ADD CONSTRAINT batch_step_execution_pkey PRIMARY KEY (step_execution_id);
+
+
+--
+-- TOC entry 3207 (class 2606 OID 26286)
+-- Name: derechohambientes derechohabientes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.derechohambientes
+    ADD CONSTRAINT derechohabientes_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3211 (class 2606 OID 25669)
+-- Name: batch_job_instance job_inst_un; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_instance
+    ADD CONSTRAINT job_inst_un UNIQUE (job_name, job_key);
+
+
+--
+-- TOC entry 3226 (class 2606 OID 25723)
+-- Name: batch_job_execution_context job_exec_ctx_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution_context
+    ADD CONSTRAINT job_exec_ctx_fk FOREIGN KEY (job_execution_id) REFERENCES public.batch_job_execution(job_execution_id);
+
+
+--
+-- TOC entry 3223 (class 2606 OID 25687)
+-- Name: batch_job_execution_params job_exec_params_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution_params
+    ADD CONSTRAINT job_exec_params_fk FOREIGN KEY (job_execution_id) REFERENCES public.batch_job_execution(job_execution_id);
+
+
+--
+-- TOC entry 3224 (class 2606 OID 25699)
+-- Name: batch_step_execution job_exec_step_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution
+    ADD CONSTRAINT job_exec_step_fk FOREIGN KEY (job_execution_id) REFERENCES public.batch_job_execution(job_execution_id);
+
+
+--
+-- TOC entry 3222 (class 2606 OID 25677)
+-- Name: batch_job_execution job_inst_exec_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution
+    ADD CONSTRAINT job_inst_exec_fk FOREIGN KEY (job_instance_id) REFERENCES public.batch_job_instance(job_instance_id);
+
+
+--
+-- TOC entry 3225 (class 2606 OID 25711)
+-- Name: batch_step_execution_context step_exec_ctx_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution_context
+    ADD CONSTRAINT step_exec_ctx_fk FOREIGN KEY (step_execution_id) REFERENCES public.batch_step_execution(step_execution_id);
+
+
+-- Completed on 2026-05-05 23:48:39
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ derechohabitante_prod.sql
@@ -0,0 +1,368 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.14
+-- Dumped by pg_dump version 16.0
+
+-- Started on 2026-05-05 23:49:45
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 216 (class 1259 OID 25742)
+-- Name: batch_error_logs; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_error_logs (
+    id integer NOT NULL,
+    job_name character varying(100),
+    file_name character varying(255),
+    raw_data text,
+    error_message text,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.batch_error_logs OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 25741)
+-- Name: batch_error_logs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_error_logs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_error_logs_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 3374 (class 0 OID 0)
+-- Dependencies: 215
+-- Name: batch_error_logs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.batch_error_logs_id_seq OWNED BY public.batch_error_logs.id;
+
+
+--
+-- TOC entry 218 (class 1259 OID 25775)
+-- Name: batch_job_execution; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_execution (
+    job_execution_id bigint NOT NULL,
+    version bigint,
+    job_instance_id bigint NOT NULL,
+    create_time timestamp without time zone NOT NULL,
+    start_time timestamp without time zone,
+    end_time timestamp without time zone,
+    status character varying(10),
+    exit_code character varying(2500),
+    exit_message character varying(2500),
+    last_updated timestamp without time zone
+);
+
+
+ALTER TABLE public.batch_job_execution OWNER TO postgres;
+
+--
+-- TOC entry 222 (class 1259 OID 25821)
+-- Name: batch_job_execution_context; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_execution_context (
+    job_execution_id bigint NOT NULL,
+    short_context character varying(2500) NOT NULL,
+    serialized_context text
+);
+
+
+ALTER TABLE public.batch_job_execution_context OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 25787)
+-- Name: batch_job_execution_params; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_execution_params (
+    job_execution_id bigint NOT NULL,
+    parameter_name character varying(100) NOT NULL,
+    parameter_type character varying(100) NOT NULL,
+    parameter_value character varying(2500),
+    identifying character(1) NOT NULL
+);
+
+
+ALTER TABLE public.batch_job_execution_params OWNER TO postgres;
+
+--
+-- TOC entry 224 (class 1259 OID 25834)
+-- Name: batch_job_execution_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_job_execution_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_job_execution_seq OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 25768)
+-- Name: batch_job_instance; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_job_instance (
+    job_instance_id bigint NOT NULL,
+    version bigint,
+    job_name character varying(100) NOT NULL,
+    job_key character varying(32) NOT NULL
+);
+
+
+ALTER TABLE public.batch_job_instance OWNER TO postgres;
+
+--
+-- TOC entry 225 (class 1259 OID 25835)
+-- Name: batch_job_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_job_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_job_seq OWNER TO postgres;
+
+--
+-- TOC entry 220 (class 1259 OID 25797)
+-- Name: batch_step_execution; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_step_execution (
+    step_execution_id bigint NOT NULL,
+    version bigint NOT NULL,
+    step_name character varying(100) NOT NULL,
+    job_execution_id bigint NOT NULL,
+    create_time timestamp without time zone NOT NULL,
+    start_time timestamp without time zone,
+    end_time timestamp without time zone,
+    status character varying(10),
+    commit_count bigint,
+    read_count bigint,
+    filter_count bigint,
+    write_count bigint,
+    read_skip_count bigint,
+    write_skip_count bigint,
+    process_skip_count bigint,
+    rollback_count bigint,
+    exit_code character varying(2500),
+    exit_message character varying(2500),
+    last_updated timestamp without time zone
+);
+
+
+ALTER TABLE public.batch_step_execution OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 25809)
+-- Name: batch_step_execution_context; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.batch_step_execution_context (
+    step_execution_id bigint NOT NULL,
+    short_context character varying(2500) NOT NULL,
+    serialized_context text
+);
+
+
+ALTER TABLE public.batch_step_execution_context OWNER TO postgres;
+
+--
+-- TOC entry 223 (class 1259 OID 25833)
+-- Name: batch_step_execution_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.batch_step_execution_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.batch_step_execution_seq OWNER TO postgres;
+
+--
+-- TOC entry 214 (class 1259 OID 25634)
+-- Name: derechohabitante; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.derechohabitante (
+    id bigint NOT NULL,
+    nombre character varying(100) NOT NULL,
+    ciudad character varying(100),
+    importe numeric(19,2),
+    cuenta character varying(16) NOT NULL
+);
+
+
+ALTER TABLE public.derechohabitante OWNER TO postgres;
+
+--
+-- TOC entry 3204 (class 2604 OID 25745)
+-- Name: batch_error_logs id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_error_logs ALTER COLUMN id SET DEFAULT nextval('public.batch_error_logs_id_seq'::regclass);
+
+
+--
+-- TOC entry 3209 (class 2606 OID 25750)
+-- Name: batch_error_logs batch_error_logs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_error_logs
+    ADD CONSTRAINT batch_error_logs_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3221 (class 2606 OID 25827)
+-- Name: batch_job_execution_context batch_job_execution_context_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution_context
+    ADD CONSTRAINT batch_job_execution_context_pkey PRIMARY KEY (job_execution_id);
+
+
+--
+-- TOC entry 3215 (class 2606 OID 25781)
+-- Name: batch_job_execution batch_job_execution_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution
+    ADD CONSTRAINT batch_job_execution_pkey PRIMARY KEY (job_execution_id);
+
+
+--
+-- TOC entry 3211 (class 2606 OID 25772)
+-- Name: batch_job_instance batch_job_instance_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_instance
+    ADD CONSTRAINT batch_job_instance_pkey PRIMARY KEY (job_instance_id);
+
+
+--
+-- TOC entry 3219 (class 2606 OID 25815)
+-- Name: batch_step_execution_context batch_step_execution_context_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution_context
+    ADD CONSTRAINT batch_step_execution_context_pkey PRIMARY KEY (step_execution_id);
+
+
+--
+-- TOC entry 3217 (class 2606 OID 25803)
+-- Name: batch_step_execution batch_step_execution_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution
+    ADD CONSTRAINT batch_step_execution_pkey PRIMARY KEY (step_execution_id);
+
+
+--
+-- TOC entry 3207 (class 2606 OID 25638)
+-- Name: derechohabitante derechohabientes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.derechohabitante
+    ADD CONSTRAINT derechohabientes_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 3213 (class 2606 OID 25774)
+-- Name: batch_job_instance job_inst_un; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_instance
+    ADD CONSTRAINT job_inst_un UNIQUE (job_name, job_key);
+
+
+--
+-- TOC entry 3226 (class 2606 OID 25828)
+-- Name: batch_job_execution_context job_exec_ctx_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution_context
+    ADD CONSTRAINT job_exec_ctx_fk FOREIGN KEY (job_execution_id) REFERENCES public.batch_job_execution(job_execution_id);
+
+
+--
+-- TOC entry 3223 (class 2606 OID 25792)
+-- Name: batch_job_execution_params job_exec_params_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution_params
+    ADD CONSTRAINT job_exec_params_fk FOREIGN KEY (job_execution_id) REFERENCES public.batch_job_execution(job_execution_id);
+
+
+--
+-- TOC entry 3224 (class 2606 OID 25804)
+-- Name: batch_step_execution job_exec_step_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution
+    ADD CONSTRAINT job_exec_step_fk FOREIGN KEY (job_execution_id) REFERENCES public.batch_job_execution(job_execution_id);
+
+
+--
+-- TOC entry 3222 (class 2606 OID 25782)
+-- Name: batch_job_execution job_inst_exec_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_job_execution
+    ADD CONSTRAINT job_inst_exec_fk FOREIGN KEY (job_instance_id) REFERENCES public.batch_job_instance(job_instance_id);
+
+
+--
+-- TOC entry 3225 (class 2606 OID 25816)
+-- Name: batch_step_execution_context step_exec_ctx_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.batch_step_execution_context
+    ADD CONSTRAINT step_exec_ctx_fk FOREIGN KEY (step_execution_id) REFERENCES public.batch_step_execution(step_execution_id);
+
+
+-- Completed on 2026-05-05 23:49:45
+
+--
+-- PostgreSQL database dump complete
+--
+
